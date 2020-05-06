e3baa17f6239f111869176c22a51e37c622346e6
sugyeong Kim <s2018s25@e-mirim.hs.kr>
2020-05-06 16:02:31 +0900

    문제 5

--- chinesezodiac/Program.cs
+++ chinesezodiac/Program.cs
@@ -10,7 +10,49 @@ namespace chinesezodiac
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("당신의 태어난 년도(ex 2002)>> ");
+            int year = int.Parse(Console.ReadLine());
+
+            switch (year % 12)
+            {
+                case 0:
+                    Console.WriteLine("당신은 원숭이 띠 입니다~");
+                    break;
+                case 1:
+                    Console.WriteLine("당신은 닭 띠 입니다~");
+                    break;
+                case 2:
+                    Console.WriteLine("당신은 개 띠 입니다~");
+                    break;
+                case 3:
+                    Console.WriteLine("당신은 돼지 띠 입니다~");
+                    break;
+                case 4:
+                    Console.WriteLine("당신은 쥐 띠 입니다~");
+                    break;
+                case 5:
+                    Console.WriteLine("당신은 소 띠 입니다~");
+                    break;
+                case 6:
+                    Console.WriteLine("당신은 호랑이 띠 입니다~");
+                    break;
+                case 7:
+                    Console.WriteLine("당신은 토끼 띠 입니다~");
+                    break;
+                case 8:
+                    Console.WriteLine("당신은 용 띠 입니다~");
+                    break;
+                case 9:
+                    Console.WriteLine("당신은 뱀 띠 입니다~");
+                    break;
+                case 10:
+                    Console.WriteLine("당신은 말 띠 입니다~");
+                    break;
+                case 11:
+                    Console.WriteLine("당신은 양 띠 입니다~");
+                    break;
+
+            }
         }
     }
 }
